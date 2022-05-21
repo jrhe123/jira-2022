69b3136454917b48ea3b54fb426d8a64f443027d
jrhe1213 <jiarong1213@gmail.com>
2022-05-21 18:22:56 -0400

    docs: add example for utility parameter

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -33,3 +33,49 @@ export const ProjectListScreen = () => {
     </div>
   );
 };
+
+/**
+ * 
+ * most case interface <-> type
+  interface Person {
+    name: string;
+  }
+  type Person = { name: string }
+  
+  const abc: Person = {
+    name: "roytest"
+  }
+
+ *
+ * some case cannot
+case 1:
+  type MyVar = string | number;
+  let myVar: MyVar = "1"
+  let myVar: MyVar = 1
+
+case 2: utility type
+
+2.1 reuse type / interface defined above
+  Parameters<typeof http>
+
+2.2 reuse defined type partially
+  Partial<Person>
+
+  type Person = {
+    name: string;
+    age: number;
+  }
+  const personWithAge: Partial<Person> = {
+    age: 1
+  }
+
+2.3 omit defined type, delete one / more properties
+  type Person = {
+    name: string;
+    age: number;
+  }
+  const hideNameInfo: Omit<Person, 'name'> = {
+    age: 123
+  }
+  const hideAllInfo: Omit<Person, 'name' | 'age'> = {}
+ */
